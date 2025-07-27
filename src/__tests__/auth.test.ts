0c143a6725db539c91326542e06115d0a7dd124b
Olanrewaju <lawallanre.abbahouse@gmail.com>
2025-07-27 19:06:31 +0100

    implemented the auth

--- src/__tests__/auth.test.ts
+++ src/__tests__/auth.test.ts
@@ -64,4 +64,39 @@ describe('Auth Endpoints', () => {
     expect(userInDb).not.toBeNull();
     expect(userInDb?.name).toBe('Test User');
   });
+
+  it('should login a user and return a token and message', async () => {
+    const res = await request(app).post('/api/auth/login').send({
+      identifier: 'test@example.com',
+      password: 'Password123',
+    });
+
+    expect(res.status).toBe(200);
+    expect(res.body.token).toBeDefined();
+    expect(res.body.message).toBe('Login successful.');
+    console.log(res.body.token);
+    console.log(res.body.message);
+
+    const userInDb = await prisma.user.findUnique({
+      where: { email: 'test@example.com' },
+    });
+
+    expect(userInDb).not.toBeNull();
+  });
+
+  it('should not login a user with invalid credentials', async () => {
+    const res = await request(app).post('/api/auth/login').send({
+      identifier: 'test@example.com',
+      password: 'invalid',
+    });
+
+    expect(res.status).toBe(400);
+    expect(res.body.error).toBe('Invalid credentials');
+
+    const userInDb = await prisma.user.findUnique({
+      where: { email: 'test@example.com' },
+    });
+
+    expect(userInDb).not.toBeNull();
+  });
 });
